d8ef1b7d2a8affe38ddb6d750b8fe4a153e339ae
AkramanSou <oumar.souleimanov@gmail>
2026-03-24 11:51:01 +0100

    8.2 a tester

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,15 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './interceptors/auth-interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient() // A AJOUTER POUR ACTIVATION DU CLIENT HTTP
+    provideHttpClient(
+      withInterceptors([authInterceptor]) // A ajouter
+    )
   ]
 };
